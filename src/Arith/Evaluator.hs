fc6b9eee5755298ccc19c2dbe5daa6b5412e7eb4
Vincent Perez <perezv815@gmail.com>
2019-11-05 17:10:52 -0500

    minor cleanup

--- src/Arith/Evaluator.hs
+++ src/Arith/Evaluator.hs
@@ -18,11 +18,12 @@ eval1Step (T_PRED T_ZERO) = T_ZERO -- E-PREDZERO
 eval1Step (T_PRED (T_SUCC t)) = eval1Step t -- E-PREDSUCC
 eval1Step (T_PRED t) = T_PRED (eval1Step t) -- E-PRED
 eval1Step (T_IS_ZERO T_ZERO) = T_TRUE -- E-ISZEROZERO
-eval1Step (T_IS_ZERO (T_SUCC t)) = T_FALSE -- E-ISZEROSUCC
+eval1Step (T_IS_ZERO (T_SUCC _)) = T_FALSE -- E-ISZEROSUCC
 eval1Step (T_IS_ZERO t) = T_IS_ZERO (eval1Step t) -- E-ISZERO
 eval1Step term = term -- already in normal form, nothing to do
 
 -- everything below is only needed for the eval big step mode of evaluation
+-- to activate the big step mode, just redefine `eval` as `eval = evalBigStep`
 isNumericalValue :: Term -> Bool
 isNumericalValue T_ZERO = True
 isNumericalValue (T_SUCC t) = isNumericalValue t
@@ -39,6 +40,7 @@ isValue t = isNumericalValue t || isBooleanValue t
 evalBigStep :: Term -> Term
 evalBigStep t
   | isValue t = t -- B-Value
+  -- | otherwise = t -- impossible to further evaluate
 evalBigStep (T_IF_THEN_ELSE t1 t2 t3) =
   let cond = evalBigStep t1
    in if cond == T_TRUE
@@ -50,8 +52,10 @@ evalBigStep (T_PRED t) =
    in case value of
         T_ZERO -> T_ZERO -- B-PREDZERO
         T_SUCC v -> v -- BPREDSUCC
+        t' -> T_PRED t' -- impossible to further evaluate
 evalBigStep (T_IS_ZERO t) =
   let value = evalBigStep t
    in case value of
         T_ZERO -> T_TRUE -- B-ISZEROZERO
-        T_SUCC v -> T_FALSE -- BISZEROSUCC
+        T_SUCC _ -> T_FALSE -- B-ISZEROSUCC
+        t' -> T_IS_ZERO t' -- impossible to further evaluate
