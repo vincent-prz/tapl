3e958504c7f81eca0ab40f25a59214a7bb5a174b
Vincent Perez <perezv815@gmail.com>
2019-11-28 12:20:30 -0500

    add possibility to switch eval mode in repl

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,36 @@ import System.Console.Haskeline
 import Untyped.Evaluator
 import Untyped.Parser
 
-processInput :: String -> String
-processInput input =
+processInput :: EvaluationStrategy -> String -> String
+processInput evalStrat input =
   let parseResult = Untyped.Parser.fullParser input
    in case parseResult of
         Left err -> show err
-        Right t -> reduceTerm t
+        Right t -> reduceTerm evalStrat t
 
-reduceTerm :: Term -> String
-reduceTerm t =
-  case evalBeta t of
+reduceTerm :: EvaluationStrategy -> Term -> String
+reduceTerm evalStrat t =
+  case evalWithStrategy evalStrat t of
     Left err -> show err
     Right t' -> show t'
 
 main :: IO ()
-main = putStrLn "Untyped lambda calculus REPL" >> runInputT defaultSettings loop
+main =
+  putStrLn "Untyped lambda calculus REPL" >>
+  runInputT defaultSettings (loop CallByValue)
   where
-    loop :: InputT IO ()
-    loop = do
+    loop :: EvaluationStrategy -> InputT IO ()
+    loop evalStrat = do
       minput <- getInputLine "> "
       case minput of
         Nothing -> return ()
         Just "quit" -> return ()
+        Just ":cbv" -> do
+          outputStrLn "Switching to call by value evaluation mode"
+          loop CallByValue
+        Just ":beta" -> do
+          outputStrLn "Switching to full beta reduction evaluation mode"
+          loop FullBeta
         Just input -> do
-          outputStrLn $ processInput input
-          loop
+          outputStrLn $ processInput evalStrat input
+          loop evalStrat
