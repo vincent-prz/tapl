a39a0cd137103254476ee3af8cd9d77e5ba8ce8b
Vincent Perez <perezv815@gmail.com>
2019-12-05 18:15:53 -0500

    add tutorial

--- app/Main.hs
+++ app/Main.hs
@@ -12,9 +12,15 @@ data Options = Options
   , strategy :: EvaluationStrategy
   } deriving (Eq)
 
+data TutorialState
+  = TutorialOff
+  | Level Int
+  deriving (Eq)
+
 data Model = Model
   { opts :: Options
   , input :: MisoString
+  , tutorialState :: TutorialState
   } deriving (Eq)
 
 -- | Sum type for application events
@@ -22,6 +28,8 @@ data Action
   = ChangeInput MisoString
   | ChangeVerbose Bool
   | ChangeEvalStrategy EvaluationStrategy
+  | ToggleTutorial
+  | NextLevel
   | NoOp
   deriving (Eq)
 
@@ -61,7 +69,12 @@ main :: IO ()
 main = startApp App {..}
   where
     initialAction = NoOp
-    model = Model {opts = defaultOpts, input = "(\\x.x) \\y.y"}
+    model =
+      Model
+        { opts = defaultOpts
+        , input = "(\\x.x) \\y.y"
+        , tutorialState = TutorialOff
+        }
     update = updateModel -- update function
     view = viewModel -- view function
     events = defaultEvents -- default delegated events
@@ -72,12 +85,60 @@ main = startApp App {..}
 updateModel :: Action -> Model -> Effect Action Model
 updateModel action m =
   case action of
-    ChangeInput newInput -> noEff $ Model {opts = opts m, input = newInput}
-    ChangeVerbose v ->
-      noEff $ Model {opts = Options v (strategy (opts m)), input = input m}
+    ChangeInput newInput -> noEff $ m {input = newInput}
+    ChangeVerbose v -> noEff $ m {opts = Options v (strategy (opts m))}
     ChangeEvalStrategy strat ->
-      noEff $ Model {opts = Options (verbose (opts m)) strat, input = input m}
+      noEff $ m {opts = Options (verbose (opts m)) strat}
+    ToggleTutorial ->
+      noEff $
+      m
+        { tutorialState = toggleTutorialState (tutorialState m)
+        , input =
+            if tutorialState m == TutorialOff
+              then getTutorialInput (Level 0)
+              else input m
+        }
+    NextLevel ->
+      let nl = nextLevel (tutorialState m)
+       in noEff $ m {tutorialState = nl, input = getTutorialInput nl}
     NoOp -> noEff m
+
+nextLevel :: TutorialState -> TutorialState
+nextLevel TutorialOff = TutorialOff
+nextLevel (Level n) = Level (n + 1)
+
+toggleTutorialState :: TutorialState -> TutorialState
+toggleTutorialState TutorialOff = Level 0
+toggleTutorialState _ = TutorialOff
+
+tutorialButtonLabel :: TutorialState -> MisoString
+tutorialButtonLabel TutorialOff = "Start Tutorial"
+tutorialButtonLabel (Level _) = "Stop Tutorial"
+
+isTutorialOn :: TutorialState -> Bool
+isTutorialOn = (/=) TutorialOff
+
+getTutorialInput :: TutorialState -> MisoString
+getTutorialInput TutorialOff = ""
+getTutorialInput (Level 0) = "\\x.x"
+getTutorialInput (Level 1) = "\\x.x x"
+getTutorialInput (Level 2) = "(\\x.x) (\\x.x x)"
+getTutorialInput (Level 3) = "\\x.\\y.y"
+getTutorialInput (Level 4) = "(\\x.\\y.y) (\\x.x) \\z.z"
+getTutorialInput (Level 5) = "\\x.\\y.\\z.(x y) z"
+getTutorialInput (Level 6) = "\\x.x \\y.y"
+
+viewTutorialText :: TutorialState -> View Action
+viewTutorialText TutorialOff = div_ [] []
+viewTutorialText (Level 0) = div_ [] [p_ [] ["simplest value"]]
+viewTutorialText (Level 1) = div_ [] [p_ [] ["another example"]]
+viewTutorialText (Level 2) = div_ [] [p_ [] ["application"]]
+viewTutorialText (Level 3) = div_ [] [p_ [] ["2 parameters"]]
+viewTutorialText (Level 4) = div_ [] [p_ [] ["2 parameters applied"]]
+viewTutorialText (Level 5) =
+  div_ [] [p_ [] ["left associativity of abstractions"]]
+viewTutorialText (Level 6) =
+  div_ [] [p_ [] ["abstractions extend as far right as possible"]]
 
 viewSingleStep :: String -> View Action
 viewSingleStep step = p_ [] [text $ toMisoString step]
@@ -113,6 +174,14 @@ viewModel m =
                 ["Full Beta reduction"]
             ]
         ]
+    , div_
+        []
+        [ button_
+            [onClick ToggleTutorial]
+            [text $ tutorialButtonLabel (tutorialState m)]
+        , button_ [onClick NextLevel] ["Next"]
+        ]
+    , viewTutorialText (tutorialState m)
     , textarea_ [cols_ "50", defaultValue_ (input m), onInput ChangeInput] []
     , div_ [] (map viewSingleStep (quieter (opts m) (fromMisoString $ input m)))
     ]
