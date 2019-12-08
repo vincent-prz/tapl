399e8507bc1d355c63085ea7a3e62d6fbe981803
Vincent Perez <perezv815@gmail.com>
2019-12-07 19:55:41 -0500

    make repl stateful to account for declared variables

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Control.Monad.State
 import Data.List (intercalate)
+import qualified Data.Map as Map
 import System.Console.Haskeline
 import Untyped.Evaluator
 import Untyped.Parser
@@ -13,42 +15,44 @@ data Options = Options
 defaultOptions :: Options
 defaultOptions = Options False CallByValue
 
-processInput :: Options -> String -> String
+processInput :: Options -> String -> State Context String
 processInput opts input =
   let parseResult = Untyped.Parser.fullParser input
    in case parseResult of
-        Left err -> show err
+        Left err -> return $ show err
         Right program -> runProgram opts program
 
-runProgram :: Options -> Program -> String
-runProgram opts program =
-  case evalProgram (strategy opts) program of
-    Left err -> show err
-    Right t' -> show t'
+runProgram :: Options -> Program -> State Context String
+runProgram opts program = do
+  result <- evalProgramWithContext (strategy opts) program
+  case result of
+    Left err -> return $ show err
+    Right t -> return $ show t
 
 main :: IO ()
 main =
   putStrLn "Untyped lambda calculus REPL" >>
-  runInputT defaultSettings (loop defaultOptions)
+  runInputT defaultSettings (loop defaultOptions Map.empty)
   where
-    loop :: Options -> InputT IO ()
-    loop opts = do
+    loop :: Options -> Context -> InputT IO ()
+    loop opts c = do
       minput <- getInputLine "> "
       case minput of
         Nothing -> return ()
         Just ":quit" -> return ()
         Just ":cbv" -> do
           outputStrLn "Switching to call by value evaluation mode."
-          loop (Options (verbose opts) CallByValue)
+          loop (Options (verbose opts) CallByValue) c
         Just ":beta" -> do
           outputStrLn "Switching to full beta reduction evaluation mode."
-          loop (Options (verbose opts) FullBeta)
+          loop (Options (verbose opts) FullBeta) c
         Just ":verbose on" -> do
           outputStrLn "Enabling verbose mode."
-          loop (Options True (strategy opts))
+          loop (Options True (strategy opts)) c
         Just ":verbose off" -> do
           outputStrLn "Disabling verbose mode."
-          loop (Options False (strategy opts))
+          loop (Options False (strategy opts)) c
         Just input -> do
-          outputStrLn $ processInput opts input
-          loop opts
+          let (output, newC) = runState (processInput opts input) c
+          outputStrLn output
+          loop opts newC
