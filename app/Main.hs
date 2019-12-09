a5d74b2127aa088dffc439fcc2d55131eee120bc
Vincent Perez <perezv815@gmail.com>
2019-12-09 17:27:56 -0500

    propose code samples in repl, WIP

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@
 
 module Main where
 
-import TutorialText
+import Lib.Lib
 import Untyped.Evaluator
 import Untyped.Parser
 
@@ -22,27 +22,24 @@ data EvalOpts = EvalOpts
   , strategy :: EvaluationStrategy
   } deriving (Eq)
 
-data TutorialState
-  = TutorialOff
-  | Level Int
-  deriving (Eq)
-
-maxLevel :: Int
-maxLevel = 4
+data CodeSample = CodeSample
+  { title :: MisoString
+  , code :: MisoString
+  , excerpt :: MisoString
+  } deriving (Eq)
 
 data Model = Model
   { opts :: EvalOpts
   , input :: MisoString
-  , tutorialState :: TutorialState
+  , codeSampleIndex :: Int
+  , codeSample :: CodeSample
   } deriving (Eq)
 
 data Action
   = ChangeInput MisoString
   | ChangeVerbose Bool
   | ChangeEvalStrategy EvaluationStrategy
-  | ToggleTutorial
-  | PreviousLevel
-  | NextLevel
+  | ChangeSample Int
   | NoOp
   deriving (Eq)
 #ifndef __GHCJS__
@@ -66,7 +63,7 @@ valueToEvaluationStrategy v =
 
 processInput :: EvalOpts -> String -> [String]
 processInput opts input =
-  let parseResult = Untyped.Parser.fullParser input
+  let parseResult = fullParser input
    in case parseResult of
         Left err -> [show err]
         Right p -> runProgram opts p
@@ -81,12 +78,6 @@ runProgram opts t =
            Left err -> [show err]
            Right t' -> [show t']
 
-quieter :: EvalOpts -> String -> [String]
-quieter opts input =
-  if null input
-    then []
-    else processInput opts input
-
 main :: IO ()
 main = runApp $ startApp App {..}
   where
@@ -94,8 +85,9 @@ main = runApp $ startApp App {..}
     model =
       Model
         { opts = defaultOpts
-        , input = "(\\x.x) \\y.y"
-        , tutorialState = TutorialOff
+        , input = code $ Prelude.head codeSampleList
+        , codeSampleIndex = 0
+        , codeSample = Prelude.head codeSampleList
         }
     update = updateModel -- update function
     view = viewModel -- view function
@@ -103,18 +95,32 @@ main = runApp $ startApp App {..}
     subs = [] -- empty subscription list
     mountPoint = Nothing -- mount point for application (Nothing defaults to 'body')
 
-getTutorialInput :: TutorialState -> MisoString
-getTutorialInput TutorialOff = ""
-getTutorialInput (Level 0) = "\\x.x"
-getTutorialInput (Level 1) = "\\x.x x"
-getTutorialInput (Level 2) = "(\\x.x) \\x.x x"
-getTutorialInput (Level 3) = "(\\x.x) \\x.x x"
-getTutorialInput (Level 4) = "\\x.\\y.y"
-getTutorialInput (Level 5) = "(\\x.\\y.y) (\\x.x) \\z.z"
-getTutorialInput (Level 6) = "\\x.\\y.\\z.(x y) z"
-getTutorialInput (Level 7) = "\\x.x \\y.y"
-getTutorialInput (Level n) = error $ "input not configured for " ++ show n
+codeSampleList :: [CodeSample]
+codeSampleList =
+  [ CodeSample
+      { title = "The Identity function"
+      , code = "id=\\x.x\nid"
+      , excerpt = "hello"
+      }
+  , CodeSample
+      { title = "Booleans"
+      , code = "true = \\t.\\f.t\nfalse = \\t.\\f.f"
+      , excerpt = "hello"
+      }
+  ]
 
+--getTutorialInput :: TutorialState -> MisoString
+--getTutorialInput TutorialOff = ""
+--getTutorialInput (Level 0) = "\\x.x"
+--getTutorialInput (Level 1) = "\\x.x x"
+--getTutorialInput (Level 2) = "(\\x.x) \\x.x x"
+--getTutorialInput (Level 3) = "(\\x.x) \\x.x x"
+--getTutorialInput (Level 4) = "\\x.\\y.y"
+--getTutorialInput (Level 5) = "(\\x.\\y.y) (\\x.x) \\z.z"
+--getTutorialInput (Level 6) = "\\x.\\y.\\z.(x y) z"
+--getTutorialInput (Level 7) = "\\x.x \\y.y"
+--getTutorialInput (Level n) = error $ "input not configured for " ++ show n
+--
 -- FIXME: clunkyness with records
 updateModel :: Action -> Model -> Effect Action Model
 updateModel action m =
@@ -123,81 +129,32 @@ updateModel action m =
     ChangeVerbose v -> noEff $ m {opts = EvalOpts v (strategy (opts m))}
     ChangeEvalStrategy strat ->
       noEff $ m {opts = EvalOpts (verbose (opts m)) strat}
-    ToggleTutorial ->
-      noEff $
-      m
-        { tutorialState = toggleTutorialState (tutorialState m)
-        , input =
-            if tutorialState m == TutorialOff
-              then getTutorialInput (Level 0)
-              else input m
-        }
-    NextLevel ->
-      let nl = nextLevel (tutorialState m)
-       in noEff $ m {tutorialState = nl, input = getTutorialInput nl}
-    PreviousLevel ->
-      let pl = previousLevel (tutorialState m)
-       in noEff $ m {tutorialState = pl, input = getTutorialInput pl}
+    ChangeSample ind ->
+      let cs = codeSampleList !! ind
+       in noEff $ m {input = code cs, codeSampleIndex = ind, codeSample = cs}
     NoOp -> noEff m
 
-nextLevel :: TutorialState -> TutorialState
-nextLevel TutorialOff = TutorialOff
-nextLevel (Level n) = Level (n + 1)
-
-previousLevel :: TutorialState -> TutorialState
-previousLevel TutorialOff = TutorialOff
-previousLevel (Level 0) = Level 0
-previousLevel (Level n) = Level (n - 1)
-
-toggleTutorialState :: TutorialState -> TutorialState
-toggleTutorialState TutorialOff = Level 0
-toggleTutorialState _ = TutorialOff
-
-tutorialButtonLabel :: TutorialState -> MisoString
-tutorialButtonLabel TutorialOff = "Start Tutorial"
-tutorialButtonLabel (Level _) = "Stop Tutorial"
-
-displayLevel :: Int -> MisoString
-displayLevel n = toMisoString ("Level " ++ show n)
-
-viewTutorialText :: TutorialState -> View Action
-viewTutorialText TutorialOff = div_ [] []
-viewTutorialText (Level n) =
-  case (,) <$> Map.lookup n levelToTitleMap <*> Map.lookup n levelToTextMap of
-    Nothing -> error $ "title or text not defined for level " ++ show n
-    Just (title, txt) ->
-      div_
-        []
-        [ p_ [] [text (displayLevel n <> ": " <> title)]
-        , p_ [style_ paragraphStyle] [text txt]
-        ]
-
+--viewTutorialText :: TutorialState -> View Action
+--viewTutorialText TutorialOff = div_ [] []
+--viewTutorialText (Level n) =
+--  case (,) <$> Map.lookup n levelToTitleMap <*> Map.lookup n levelToTextMap of
+--    Nothing -> error $ "title or text not defined for level " ++ show n
+--    Just (title, txt) ->
+--      div_
+--        []
+--        [ p_ [] [text (displayLevel n <> ": " <> title)]
+--        , p_ [style_ paragraphStyle] [text txt]
+--        ]
+--
 paragraphStyle :: Map.Map MisoString MisoString
 paragraphStyle = Map.singleton "width" "50%"
 
---viewTutorialText (Level 1) = div_ [] [p_ [] ["another example"]]
---viewTutorialText (Level 2) = div_ [] [p_ [] ["application"]]
---viewTutorialText (Level 3) = div_ [] [p_ [] ["2 parameters"]]
---viewTutorialText (Level 4) = div_ [] [p_ [] ["2 parameters applied"]]
---viewTutorialText (Level 5) =
---  div_ [] [p_ [] ["left associativity of abstractions"]]
---viewTutorialText (Level 6) =
---  div_ [] [p_ [] ["abstractions extend as far right as possible"]]
---
-getNextButtonStyle :: TutorialState -> Map.Map MisoString MisoString
-getNextButtonStyle TutorialOff = Map.singleton "display" "none"
-getNextButtonStyle (Level n)
-  | n < maxLevel = Map.singleton "display" "inline"
-  | otherwise = Map.singleton "display" "none"
-
-getPreviousButtonStyle :: TutorialState -> Map.Map MisoString MisoString
-getPreviousButtonStyle TutorialOff = Map.singleton "display" "none"
-getPreviousButtonStyle (Level n)
-  | n > 0 = Map.singleton "display" "inline"
-  | otherwise = Map.singleton "display" "none"
-
 viewSingleStep :: String -> View Action
 viewSingleStep step = p_ [] [text $ toMisoString step]
+
+viewCodeSampleOption :: CodeSample -> Int -> View Action
+viewCodeSampleOption cs ind =
+  option_ [value_ $ toMisoString $ show ind] [text $ title cs]
 
 viewModel :: Model -> View Action
 viewModel m =
@@ -209,17 +166,10 @@ viewModel m =
         ]
     , div_
         []
-        [ button_
-            [onClick ToggleTutorial]
-            [text $ tutorialButtonLabel (tutorialState m)]
-        , button_
-            [ style_ (getPreviousButtonStyle (tutorialState m))
-            , onClick PreviousLevel
-            ]
-            ["Previous"]
-        , button_
-            [style_ (getNextButtonStyle (tutorialState m)), onClick NextLevel]
-            ["Next"]
+        [ span_ [] ["Samples"]
+        , select_
+            [on "change" valueDecoder $ ChangeSample . read . fromMisoString]
+            (mapWithIndex viewCodeSampleOption codeSampleList)
         , span_ [] ["Print all steps ?"]
         , select_
             [ on "change" valueDecoder $
@@ -241,7 +191,8 @@ viewModel m =
                 ["Full Beta reduction"]
             ]
         ]
-    , viewTutorialText (tutorialState m)
     , textarea_ [cols_ "50", defaultValue_ (input m), onInput ChangeInput] []
-    , div_ [] (map viewSingleStep (quieter (opts m) (fromMisoString $ input m)))
+    , div_
+        []
+        (map viewSingleStep (processInput (opts m) (fromMisoString $ input m)))
     ]
--- src/Lib/Lib.hs
+++ src/Lib/Lib.hs
@@ -4,3 +4,6 @@ lastMay :: [a] -> Maybe a
 lastMay [] = Nothing
 lastMay [x] = Just x
 lastMay (_:xs) = lastMay xs
+
+mapWithIndex :: (a -> Int -> b) -> [a] -> [b]
+mapWithIndex f l = zipWith f l [0 ..]
